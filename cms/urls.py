c2ba63584fd756809a9a54ba1c84fd5bfc0ebac2
AdrianJoseph <adriangj@stud.ntnu.no>
2018-09-21 12:02:53 +0200

    Added post_form

--- cms/urls.py
+++ cms/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.views.generic import ListView
-
-from cms.models import Post
-from cms.views import PostListView, DraftListView
+from cms.views import PostListView, DraftListView, PostCreateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="post_list"),
     path('drafts', DraftListView.as_view(), name="draft_list"),
+    path('new', PostCreateView.as_view(), name="post_form"),
 ]
