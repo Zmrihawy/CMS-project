91840ba5604fa86d10d62f010cb465691949eb7b
jonatan <jonaengs@gmail.com>
2018-09-21 12:17:50 +0200

    added support for publishing drafts

--- cms/urls.py
+++ cms/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.views.generic import ListView
 
 from cms.models import Post
-from cms.views import PostListView, DraftListView
+from cms.views import PostListView, DraftListView, publish_post_view
 
 urlpatterns = [
     path('', PostListView.as_view(), name="post_list"),
     path('drafts', DraftListView.as_view(), name="draft_list"),
+
+    path('<int:pk>/publish', publish_post_view, name="publish_post")
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 
 from cms.models import Post
@@ -11,4 +13,18 @@ class PostListView(ListView):
 
 class DraftListView(PostListView):
     queryset = Post.objects.filter(published=False)
-    template_name = 'post_list'
+    template_name = 'cms/post_list.html'
+
+
+########################################################
+########################################################
+
+def publish_post_view(request, pk):
+    if request.method == 'POST':
+        if request.user.groups.filter(name='editor').exists():
+            post = Post.objects.get(pk=pk)
+            post.published = True
+            post.save()
+            return HttpResponseRedirect(reverse_lazy("post_list"))
+        return HttpResponse("You do not have permission to publish")
+
