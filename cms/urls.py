ac89aff6eb75e29ded1d4b07f4bc0453cefbfe1e
Ola <olajkr@stud.ntnu.no>
2018-09-25 18:31:00 +0200

    completed user story 4, including tests

--- cms/urls.py
+++ cms/urls.py
@@ -3,13 +3,14 @@ from django.views.generic import DetailView
 
 from cms.models import Post
 from cms.populate import populate
-from cms.views import PostListView, DraftListView, publish_post_view, PostCreateView
+from cms.views import PostListView, DraftListView, publish_post_view, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="post_list"),
     path('drafts', DraftListView.as_view(), name="draft_list"),
     path('<int:pk>/publish', publish_post_view, name="publish_post"),
     path('new', PostCreateView.as_view(), name="post_form"),
+    path('<int:pk>/edit', PostUpdateView.as_view(), name="post_update"),
     path('<int:pk>', DetailView.as_view(model=Post), name="post_detail"),
 
 
