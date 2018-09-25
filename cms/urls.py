051669c5991278bd61a8abeec381a412b8fed7d6
Zaher <r.zooze@hotmail.com>
2018-09-25 16:47:41 +0200

    Added detailview for post object

--- cms/urls.py
+++ cms/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import DetailView
+
 from cms.models import Post
 from cms.views import PostListView, DraftListView, publish_post_view, PostCreateView
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path('drafts', DraftListView.as_view(), name="draft_list"),
     path('<int:pk>/publish', publish_post_view, name="publish_post"),
     path('new', PostCreateView.as_view(), name="post_form"),
+    path('<int:pk>', DetailView.as_view(model=Post), name="post_detail"),
 ]
